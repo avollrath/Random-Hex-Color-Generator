aa52a8610d724b8f85ec71bb5239a27ca8b4594e
André Vollrath <andrevollrath@Andres-MacBook-Pro.local>
2019-03-22 13:10:18 +0200

    changed design

--- js/script.js
+++ js/script.js
@@ -2,63 +2,58 @@ const btn = document.querySelector('button');
 const input = document.querySelector('input');
 const boxes = document.querySelector('.boxes');
 
-const copyToClipboard = str => {
-    const el = document.createElement('textarea');
-    el.value = event.target.innerHTML;
-    document.body.appendChild(el);
-    el.select();
-    document.execCommand('copy');
-    document.body.removeChild(el);
-    showCopiedText();
-  };
+
+btn.addEventListener("click", addBox);
 
 
-
-
-
-btn.addEventListener("click", addRanBox);
-
-addRanBox();
-
-
-function addRanBox(){
+function addBox(){
 boxes.innerHTML = '';
-let count = input.value;
-for (i= 1; i <= count; i++) {
-    let box = document.createElement("div");
+document.querySelector('p').textContent = 'Click on a color to copy it!';
+let i = 0;
+do{
+    let box = document.createElement('div');
     let color = ranHexCol();
     box.className = 'box';
     box.style.backgroundColor = color;
     box.innerHTML = color;
     box.addEventListener("click",copyToClipboard);
     boxes.appendChild(box);
+    i++;
 }
+while (i < input.value)
 }
 
 function ranHexCol() {
-var letters = '0123456789ABCDEF';
-var color = '#';
-    for (var i = 0; i < 6; i++) {
+const letters = '0123456789ABCDEF';
+let color = '#';
+    for (let i = 0; i < 6; i++) {
       color += letters[Math.floor(Math.random() * letters.length)];
     }
     return color;
 }
 
-
-
-
-function showCopiedText(){
-let message = document.createElement("h1");
+function showText(){
+let message = document.createElement('h1');
 message.innerHTML = 'COPIED!';
 message.className = 'message';
 event.target.appendChild(message);
 fadeOutEffect(message);
 }
 
+function copyToClipboard() {
+    const textArea = document.createElement('textarea');
+    textArea.value = event.target.innerHTML;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textArea);
+    showText();
+  };
 
-function fadeOutEffect(target) {
-let fadeTarget = target;
-let fadeEffect = setInterval(function () {
+
+function fadeOutEffect(fadeTarget) {
+
+setInterval(function () {
         if (!fadeTarget.style.opacity) {
             fadeTarget.style.opacity = 1;
         }
@@ -67,9 +62,9 @@ let fadeEffect = setInterval(function () {
         } else {
             clearInterval(fadeEffect);
         }
-    }, 1);
+    }, 5);
     
-    setTimeout(function(){
+setTimeout(function(){
         document.querySelector('.message').remove();
    },1500);
 }
